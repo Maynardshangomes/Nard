c305b8644bb49d8529ba25456515ac37358603d8
Maynardshangomes <maynardgomes27@gmail.com>
2024-02-08 22:39:03 +0530

    mesaage

---
+++ May/src/may/practice.java
@@ -0,0 +1,14 @@
+package may;
+
+public class practice {
+	
+	String acc_num;
+	int num_units;
+	
+	practice(String acc_num,int num_units)
+	{
+		this.acc_num=acc_num;
+		this.num_units=num_units;
+	}
+
+}
